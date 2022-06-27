8c84817c755cb78874c9711a4ce9238f48a8b3ec
Zechariah Foxx <zak.foxx@gmail.com>
2022-06-26 20:21:40 -0400

    handle form submission

--- module-4-assets/assets/js/script.js
+++ module-4-assets/assets/js/script.js
@@ -1,11 +1,13 @@
-console.dir(window.document);
-
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-buttonEl.addEventListener("click", function () {
+var createTaskHandler = function (event) {
+  event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
-});
+};
+
+formEl.addEventListener("submit", createTaskHandler);
